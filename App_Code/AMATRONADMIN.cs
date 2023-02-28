d05cd47239996dfabda46e6443d74ed75439d662
FeraMarco <fera.marco92@gmail.com>
2023-02-28 17:49:38 +0100

    rifatto abilitavenditoricorrieri con css cs e classe

---
+++ App_Code/AMATRONADMIN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class AMATRONADMIN
+{
+    public int chiaveVenditore;
+    public int chiaveCorriere;
+    DATABASE D = new DATABASE();
+
+    public AMATRONADMIN()
+    {
+ 
+    }
+
+    // Metodo per abilitare i venditori
+    public void AbilitaVenditori()
+    {
+        D.query = "spVENDITORI_Abilita";
+        D.cmd.Parameters.AddWithValue("chiave", chiaveVenditore);
+        D.EseguiSPNonRead();
+    }
+
+    // Metodo per abilitare i corrieri
+    public void AbilitaCorrieri()
+    {
+        D.query = "spCORRIERI_Abilita";
+        D.cmd.Parameters.AddWithValue("chiave", chiaveCorriere);
+        D.EseguiSPNonRead();
+    }
+}

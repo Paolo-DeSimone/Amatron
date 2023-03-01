49f6dee4582b0ca9962abbc3ee07fbdddfdffcc2
FeraMarco <fera.marco92@gmail.com>
2023-03-01 17:43:28 +0100

    cs abilita venditoricorrieri e gestione config

--- App_Code/AMATRONADMIN.cs
+++ App_Code/AMATRONADMIN.cs
@@ -8,12 +8,7 @@ using System.Data.SqlClient;
 
 public class AMATRONADMIN
 {
-    public int COSTOPRIME;
-    public int PERC1_10;
-    public int PERC11_100;
-    public int PERC101_1000;
-    public int PERC1001;
-
+ 
     public string email;
     public string PWD;
     RifAmatron.WsAmatronSoapClient A = new RifAmatron.WsAmatronSoapClient();

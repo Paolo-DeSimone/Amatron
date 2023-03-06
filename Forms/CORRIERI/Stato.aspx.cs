cb01ea832bf218e42fe2fe1f431a30e1ae34a42c
Simone Russo <simogiorusso@gmail.com>
2023-03-06 18:15:50 +0100

    finita pagina STATO.aspx

--- Forms/CORRIERI/Stato.aspx.cs
+++ Forms/CORRIERI/Stato.aspx.cs
@@ -11,6 +11,7 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         int chiave = int.Parse(Session["chiaveUSR"].ToString());
+        grdSTATO.DataBind();
     }
 
 
@@ -35,24 +36,48 @@ public partial class _Default : System.Web.UI.Page
         SPE.STATO = Session["STATO_SPEDIZIONE"].ToString();
         SPE.SPEDIZIONI_UpdateStato();
         DataTable DT = SPE.SPEDIZIONI_SelectByKey();
-        //string STATO = ;
-        
-        switch(SPE.STATO)
+        string STATO = DT.Rows[0]["STATO"].ToString();
+        EMAIL E = new EMAIL();
+        EMAIL EM = new EMAIL();
+
+        switch (STATO)
         {
             case "B":
-
+                // invio EMAIL a AMATRON e al cliente dell'ordine aggiornandolo sullo stato
+                EM.mailTo = "giorgio.zucchetti00@gmail.com";
+                EM.subject = "AGGIORNAMENTO STATO";
+                EM.body ="PRESO IN CARICO L'ORDINE(n." + "  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
+                EM.SendEmail();
+                E.mailTo = grdSTATO.SelectedRow.Cells[6].Text;
+                E.subject = "AGGIORNAMENTO STATO";
+                E.body = "Il tuo ordine è stato PRESO IN CARICO";
+                E.SendEmail();
                 break;
 
             case "C":
-
+                EM.mailTo = "giorgio.zucchetti00@gmail.com";
+                EM.subject = "AGGIORNAMENTO STATO";
+                EM.body = "IN CONSEGNA L'ORDINE(n." +"  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
+                EM.SendEmail();
+                E.mailTo = grdSTATO.SelectedRow.Cells[6].Text;
+                E.subject = "AGGIORNAMENTO STATO";
+                E.body = "Il tuo ordine è IN CONSEGNA";
+                E.SendEmail();
                 break;
 
             case "D":
-
+                EM.mailTo = "giorgio.zucchetti00@gmail.com";
+                EM.subject = "AGGIORNAMENTO STATO";
+                EM.body = "CONSEGNATO L'ORDINE(n." + "  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
+                EM.SendEmail();
+                E.mailTo = grdSTATO.SelectedRow.Cells[6].Text;
+                E.subject = "AGGIORNAMENTO STATO";
+                E.body = "Il tuo ordine è STATO CONSEGNATO";
+                E.SendEmail();
                 break;
 
         }
 
-       //DataBind();
+        DataBind();
     }
 }

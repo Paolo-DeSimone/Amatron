7f2cfcb2ceb2cce3e75a3bd3bc48c0705f7a60a2
DiegoDaggiano <diegodaggiano@gmail.com>
2023-03-06 18:15:42 +0100

    Modifiche Statistiche

--- Forms/CORRIERI/Statistiche.aspx.cs
+++ Forms/CORRIERI/Statistiche.aspx.cs
@@ -11,5 +11,7 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         int chiave = int.Parse(Session["chiaveUSR"].ToString());
+        //grigliaStatisticheCorrieri.DataBind();
     }
+    
 }

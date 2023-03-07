80888de1012f273f953e9d4bf1a259a3e5928b86
Simone Russo <simogiorusso@gmail.com>
2023-03-07 13:02:05 +0100

    con il seleziona la pagina stato va, ora sto provando ad utilizzare le checkbox e se non va è per quello

--- Forms/CORRIERI/Stato.aspx.cs
+++ Forms/CORRIERI/Stato.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,12 +13,19 @@ public partial class _Default : System.Web.UI.Page
     {
         int chiave = int.Parse(Session["chiaveUSR"].ToString());
         grdSTATO.DataBind();
+        CORRIERI C = new CORRIERI();
+        C.chiave = chiave;
+        DataTable DT = new DataTable();
+        DT = C.CORRIERI_SelectByKey();
+        string RagioneSociale = DT.Rows[0]["RagioneSociale"].ToString();
+        OrdiniCorriere.InnerHtml = "<h2>Spedizioni del Corriere " + RagioneSociale + "</h2>";
     }
 
 
-
-    protected void grdSTATO_SelectedIndexChanged(object sender, EventArgs e)
+    protected void Check_CheckedChanged(object sender, EventArgs e)
     {
+        CheckBox check = (CheckBox)sender;
+        GridViewRow row = (GridViewRow)check.NamingContainer;
         // controllo se viene selezionato la riga
         if (grdSTATO.SelectedValue == null)
         {
@@ -27,10 +35,49 @@ public partial class _Default : System.Web.UI.Page
         //faccio la session per passare la chiave
         Session["chiaveSPEDIZIONE"] = grdSTATO.SelectedValue.ToString();
         Session["STATO_SPEDIZIONE"] = grdSTATO.SelectedRow.Cells[3].Text;
+
+    }
+
+    protected void CheckALL_CheckedChanged(object sender, EventArgs e)
+    {
+        CheckBox checkALL = (CheckBox)grdSTATO.SelectedRow.FindControl("checkALL");
+        foreach (GridView row in grdSTATO.Rows)
+        {
+            CheckBox checkrow = (CheckBox)row.FindControl("Check");
+            if (checkALL.Checked == true)
+            {
+                checkALL.Checked = true;
+            }
+            else
+            {
+                checkrow.Checked = false;
+            }
+        }
+
+    }
+    protected void grdSTATO_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // controllo se viene selezionato la riga
+        //if (grdSTATO.SelectedValue == null)
+        //{
+        //    Session["chiaveSPEDIZIONE"] = null;
+        //    return;
+        //}
+        ////faccio la session per passare la chiave
+        //Session["chiaveSPEDIZIONE"] = grdSTATO.SelectedValue.ToString();
+        //Session["STATO_SPEDIZIONE"] = grdSTATO.SelectedRow.Cells[3].Text;
     }
 
     protected void btnStato_Click(object sender, EventArgs e)
     {
+        if (grdSTATO.SelectedRow.Cells[3].Text == "D")
+        {
+            string scripterr = @"notifyError('Prodotto già consegnato')"; //messaggio di errore
+            ScriptManager.RegisterStartupScript(this, GetType(), "btnStato_Click", scripterr, true);
+            return;
+        }
+
+
         SPEDIZIONI SPE = new SPEDIZIONI();
         SPE.chiave = int.Parse(Session["chiaveSPEDIZIONE"].ToString());
         SPE.STATO = Session["STATO_SPEDIZIONE"].ToString();
@@ -46,23 +93,29 @@ public partial class _Default : System.Web.UI.Page
                 // invio EMAIL a AMATRON e al cliente dell'ordine aggiornandolo sullo stato
                 EM.mailTo = "giorgio.zucchetti00@gmail.com";
                 EM.subject = "AGGIORNAMENTO STATO";
-                EM.body ="PRESO IN CARICO L'ORDINE(n." + "  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
+                EM.body = "PRESO IN CARICO L'ORDINE(n." + "  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
                 EM.SendEmail();
                 E.mailTo = grdSTATO.SelectedRow.Cells[6].Text;
                 E.subject = "AGGIORNAMENTO STATO";
                 E.body = "Il tuo ordine è stato PRESO IN CARICO";
                 E.SendEmail();
+                string scripter1 = @"notifySuccess('Consegna Presa in Carico')"; //messaggio di stato
+                ScriptManager.RegisterStartupScript(this, GetType(), "btnStato_Click", scripter1, true);
+
                 break;
 
             case "C":
                 EM.mailTo = "giorgio.zucchetti00@gmail.com";
                 EM.subject = "AGGIORNAMENTO STATO";
-                EM.body = "IN CONSEGNA L'ORDINE(n." +"  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
+                EM.body = "IN CONSEGNA L'ORDINE(n." + "  " + grdSTATO.SelectedRow.Cells[7].Text + ") di " + "  " + grdSTATO.SelectedRow.Cells[6].Text;
                 EM.SendEmail();
                 E.mailTo = grdSTATO.SelectedRow.Cells[6].Text;
                 E.subject = "AGGIORNAMENTO STATO";
                 E.body = "Il tuo ordine è IN CONSEGNA";
                 E.SendEmail();
+                string scripter2 = @"notifySuccess('Prodotto in Consegna')"; //messaggio di stato
+                ScriptManager.RegisterStartupScript(this, GetType(), "btnStato_Click", scripter2, true);
+
                 break;
 
             case "D":
@@ -74,10 +127,14 @@ public partial class _Default : System.Web.UI.Page
                 E.subject = "AGGIORNAMENTO STATO";
                 E.body = "Il tuo ordine è STATO CONSEGNATO";
                 E.SendEmail();
+                string scripter3 = @"notifySuccess('Prodotto Consegnato')"; //messaggio di stato
+                ScriptManager.RegisterStartupScript(this, GetType(), "btnStato_Click", scripter3, true);
                 break;
 
         }
 
         DataBind();
     }
+
+    
 }

9dad0bc2f9665b0f4482cf4e63f8e13a9ae870bb
OtmicusRX <matthew_27@live.it>
2023-03-07 17:50:39 +0100

    reset pagina ReportAMATRON per droppare chartjs

--- Forms/AMATRON/ReportAMATRON.aspx.cs
+++ Forms/AMATRON/ReportAMATRON.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Dispatcher;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -12,14 +13,19 @@ public partial class ReportAMATRON : System.Web.UI.Page
     {
         if (!IsPostBack)
         {
-            string valore = "12, 19, 3, 5, 2, 3, 4, 8, 23, 26, 3, 16";
-            Text1.Value = valore;
+            //string valore = "12, 19, 3, 5, 2, 3, 4, 8, 23, 26, 3, 16";
+            //Text1.Value = valore;
+            //int[] valore = new int[12] { 12, 19, 3, 5, 2, 3, 4, 8, 23, 26, 3, 16 };
+            //foreach (int i in valore)
+            //{
+            //    Text1.Value = valore[i];
+            //}
         }
     }
 
-    //public static string dataMensile()
-    //{
-    //    string valore = "12, 19, 3, 5, 2, 3, 4, 8, 23, 26, 3, 16";
-    //    return valore;
-    //}
+    public static int[] dataMensile()
+    {
+        int[] valore = new int[12] { 12, 19, 3, 5, 2, 3, 4, 8, 23, 26, 3, 16 };
+        return valore;
+    }
 }

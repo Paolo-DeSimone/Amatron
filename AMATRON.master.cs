e19301bb656e7510af5f1a54ace711375849626a
Paolo De Simone - Kappas.it <paolods22299@gmail.com>
2023-03-08 17:25:33 +0100

    Finish CSS cart. To finish SQL and C# to delete item and to go to cart page

--- AMATRON.master.cs
+++ AMATRON.master.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Services;
 using System.Web.UI;
@@ -63,36 +64,41 @@ public partial class AMATRON : System.Web.UI.MasterPage
         for (int i = 0; i < DT.Rows.Count; i++)
         {
             s +=
-            "<div class=\"card mx-1 my-1\">" +
+            "<div id=\"itemInCart\"class=\"card mx-1 my-1\">" +
             "<div class=\"card-body \" style=\"padding: 5px; color: rgb(0,0,0);\r\n font-weight: 600;\r\n\">" +
             "<div class=\"row\" style=\"margin: 0px; padding: 0px;\">" +
             "<div class=\"row\" style=\"margin: 0px; padding: 0px;\">" +
             "" +
             " <div class=\"col-sm-4\">" +
-            "<a href=\"Forms/PRODOTTI/PaginaProdotto.aspx?c="+ DT.Rows[i]["chiavePRODOTTO"].ToString() + "\" class=\"w3-bar-item w3-button\">" +
-            "<img src=\"/Img.ashx?c=" + DT.Rows[i]["chiavePRODOTTO"].ToString() +"\" class=\"w-50 h-50\" />" +
-            "</a>" +
+                "<a href=\"Forms/PRODOTTI/PaginaProdotto.aspx?c="+ DT.Rows[i]["chiavePRODOTTO"].ToString() + "\" class=\"w3-bar-item w3-button\">" +
+                "<img src=\"/Img.ashx?c=" + DT.Rows[i]["chiavePRODOTTO"].ToString() +"\" class=\"w-50 h-50\" />" +
+                "</a>" +
             "</div>" +
             "" +
             " <div class=\"col-sm-5\">" +
             "<p style=\"margin: 4px;\"></p>" +
             "" +
             "<div>" + //DIV di apertura
-            "<div>" + DT.Rows[i]["TITOLOprodotto"] + "</div>" +
-            "</div>" +
-            "<div style=\"padding: 5px; color: rgb(0,0,0);\r\n font-weight: 500;\r\n\">" +DT.Rows[i]["DESCRIZIONEprodotto"].ToString() + "</div>" +
-            "</div>" + //DIV di chiusura
-            "" +
-            "<div style=\"padding: 0px;\" class=\"col-sm-3\">" +
+                "<div>" + DT.Rows[i]["TITOLOprodotto"] + "</div>" +
+                "</div>" +
+                "<div style=\"color: rgb(0,0,0);\r\n font-weight: 500;\r\n\">" +DT.Rows[i]["DESCRIZIONEprodotto"].ToString() + "</div>" +
+                "</div>" + //DIV di chiusura
+                "" +
+                "<div style=\"padding: 0px;\" class=\"col-sm-3\">" +
             "<div>" + //DIV di apertura
             "" +
-            "<button onclick=\"closeRightMenu()\"  class=\"w3-bar-item w3-button w3-large\" style=\"padding: 0px; margin-left: 100px;\" >" +
-            "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3\" viewBox=\"0 0 16 16\">\r\n  <path d=\"M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z\"/>\r\n</svg>" +
+            "<button onclick=\"DiscardItemInCart()\" class=\"w3-bar-item w3-button w3-large\" style=\"padding: 0px; margin-left: 100px;\" >" +
+                "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3\" viewBox=\"0 0 16 16\">\r\n  " +
+                "<path d=\"M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01" +
+                " 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01" +
+                " 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1" +
+                " .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 " +
+                "4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z\"/>\r\n</svg>" +
             "</button>" +
             "" +
-            "<div>Quantità:" + DT.Rows[i]["QTAprodotto"] + "</div>" +
-            "</div>" +
-            "<div>Prezzo:" + DT.Rows[i]["PREZZOprodotto"].ToString() + "€</div>" +
+                "<div>Quantità:" + DT.Rows[i]["QTAprodotto"] + "</div>" +
+                "</div>" +
+                "<div>Prezzo:" + DT.Rows[i]["PREZZOprodotto"].ToString() + "€</div>" +
             "</div>" + //DIV di chiusura
             "" +
             "" +
@@ -107,6 +113,13 @@ public partial class AMATRON : System.Web.UI.MasterPage
 
     }
 
+
+    public void DiscardItemInCart()
+    {
+        CARRELLO C = new CARRELLO();
+        DataTable DT = C.SELECTBYCLIENTE();
+        // C.DELETE(DT.Rows[]chiave);
+    }
 
 
     [WebMethod/*(EnableSession = true)*/] 
